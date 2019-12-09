5b89e91a8c6b6c80e8304e631f89a4afef928b06
AndreiZn <anznobishchev@gmail.com>
2019-12-09 04:12:42 +0300

    display best, average and last reaction time

--- Experiments/Tests/1-3_Reaction_time_tests/RT_Core.m
+++ Experiments/Tests/1-3_Reaction_time_tests/RT_Core.m
@@ -6,6 +6,9 @@ CFG_test = CFG.tests{test_idx};
 
 r_c = CFG_test.radius_circle;
 c_color = CFG_test.circle_color;
+best_rt = 0;
+average_rt = 0;
+last_rt = 0;
 
 Screen('FillRect', CFG_general.win, CFG_general.bgcolor);
 Screen('Flip', CFG_general.win);
@@ -33,6 +36,7 @@ for trial_idx = 1:CFG.tests{test_idx}.num_trials
     while ~target_presented
         
         Screen('FillRect', CFG_general.win, CFG_general.bgcolor);
+        Display_RT(CFG_general, best_rt, average_rt, last_rt)
         Screen('Flip', CFG_general.win);
         
         if ~CFG_test.rand_pos
@@ -46,17 +50,23 @@ for trial_idx = 1:CFG.tests{test_idx}.num_trials
         DATA_test.target_pos(trial_idx, :) = [object_x, object_y];
         
         if GetSecs - time_last_click >= r
-              
+            
             if c_color(trial_idx) == 1 % red
                 Screen('FillOval', CFG_general.win, CFG_general.red, [object_x - r_c/2, object_y - r_c/2, object_x + r_c/2, object_y + r_c/2]);
             else % blue
                 Screen('FillOval', CFG_general.win, CFG_general.blue, [object_x - r_c/2, object_y - r_c/2, object_x + r_c/2, object_y + r_c/2]);
             end
             Screen('FrameOval', CFG_general.win, CFG_general.black, [object_x - r_c/2, object_y - r_c/2, object_x + r_c/2, object_y + r_c/2], 5);
+            Display_RT(CFG_general, best_rt, average_rt, last_rt)
+            %Screen('Flip', CFG_general.win);
             time_target_presented = Screen('Flip', CFG_general.win);
+            
+            %Display_RT(CFG_general, best_rt, average_rt)
+            %Screen('Flip', CFG_general.win);
             
             [time_click, whichButton] = Wait_user_input(CFG_general, CFG_test);
             target_presented = 1;
+            
             
             DATA_test.time{trial_idx, 1} = datestr(now,'dd-mm-yyyy HH:MM:SS FFF');
             DATA_test.reaction_time(trial_idx, 1) = round((time_click - time_target_presented) * 1000);
@@ -70,6 +80,12 @@ for trial_idx = 1:CFG.tests{test_idx}.num_trials
         Close_screen_if_escKey_pressed(CFG_general.escKey)
             
     end
+    
+    best_rt = min(DATA_test.reaction_time(1:trial_idx, 1));
+    average_rt = mean(DATA_test.reaction_time(1:trial_idx, 1));
+    last_rt = DATA_test.reaction_time(trial_idx, 1);
+    Display_RT(CFG_general, best_rt, average_rt, last_rt)
+    Screen('Flip', CFG_general.win);
 end
 
 DATA_test.num_trials = CFG_test.num_trials;
